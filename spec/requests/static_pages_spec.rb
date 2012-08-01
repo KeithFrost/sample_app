12552e1de944151f65dc3f5ba58e95f785446fa1
Keith Frost <keith.l.frost@gmail.com>
2012-08-01 19:56:57 -0400

    Add an About Us page, all the cool kids are doing it.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,4 +14,11 @@ describe "StaticPages" do
       page.should have_content('Help')
     end
   end
+
+  describe "GET /static_pages/about" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+      end
+    end
 end
