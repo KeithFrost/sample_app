a73246d319b3f4dedfca304072750ea9398e9c39
Keith Frost <keith.l.frost@gmail.com>
2012-08-17 18:43:47 -0400

    static pages without layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,24 +1,39 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "GET /static_pages/home" do
-    it "should have the content 'Sample App'" do
+  describe "Home Page" do
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
     end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => 'Sample App | Home')
+      end
   end
-  
-  describe "GET /static_pages/help" do 
-    it "should have the content 'Help'" do
+
+  describe "Help Page" do
+    it "should have the h1 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => 'Sample App | Help')
     end
   end
 
-  describe "GET /static_pages/about" do
-    it "should have the content 'About Us'" do
+  describe "About Page" do
+    it "should have the h1 'About Us'" do
       visit '/static_pages/about'
-      page.should have_content('About Us')
-      end
+      page.should have_selector('h1', :text => 'About Us')
     end
+    
+    it "should have the title 'About Us'" do 
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => 'Sample App | About Us')
+    end
+  end
 end
